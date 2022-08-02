5dfc038efa688f080ecc8262f3a6445870b2fa9a
Yasser Albogami <yalbogami@ksu.edu.sa>
2022-08-02 09:20:51 +0300

    push

---
+++ index.php
@@ -0,0 +1,10 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2638
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Monaco;}
+{\colortbl;\red255\green255\blue255;\red124\green139\blue158;\red246\green246\blue246;}
+{\*\expandedcolortbl;;\cssrgb\c55686\c61569\c68235\c85098;\cssrgb\c97255\c97255\c97255;}
+\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs28 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 <?php include_once("index.html"); ?>}
